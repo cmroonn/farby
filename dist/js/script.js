1048438eaf56170bc75349b45887f3e333efb66d
cmroonn <fallgetell@gmail.com>
2024-05-06 09:03:19 +0300

    'fixes'

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
     spaceBetween: 25,
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+      nextEl: '#nextDesigners',
+      prevEl: '#prevDesigners'
     },
     breakpoints: {
       500: {
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
     spaceBetween: 25,
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev'
+      nextEl: '#nextMasters',
+      prevEl: '#prevMasters'
     },
     breakpoints: {
       500: {
@@ -41,6 +41,71 @@ document.addEventListener("DOMContentLoaded", function () {
           btn.classList.add("active");
         }
       });
+    });
+  }
+  {
+    var buttons = document.querySelectorAll(".portfolio__nav button");
+    var items = document.querySelectorAll(".portfolio__body");
+    buttons.forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        items.forEach(function (item) {
+          item.classList.remove("active");
+          if (item.dataset.id === btn.dataset.id) {
+            buttons.forEach(function (btn2) {
+              return btn2.classList.remove("active");
+            });
+            item.classList.add("active");
+            btn.classList.add("active");
+          }
+        });
+      });
+    });
+  }
+  {
+    var _buttons = document.querySelectorAll(".faq__buttons button");
+    var _items = document.querySelectorAll(".faq__text-area p");
+    _buttons.forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        _items.forEach(function (item) {
+          item.classList.remove("active");
+          if (item.dataset.id === btn.dataset.id) {
+            _buttons.forEach(function (btn2) {
+              return btn2.classList.remove("active");
+            });
+            item.classList.add("active");
+            btn.classList.add("active");
+          }
+        });
+      });
+    });
+  }
+  {
+    var openPopup = function openPopup(elemsClass, popupId) {
+      var elems = document.querySelectorAll(".".concat(elemsClass));
+      var popup = document.getElementById(popupId);
+      elems.forEach(function (elem) {
+        elem.addEventListener("click", function (e) {
+          e.preventDefault();
+          popup.classList.add("active");
+        });
+      });
+    };
+    openPopup('open-popup-call', 'popupCall');
+    openPopup('open-popup-calc', 'popupCalc');
+    openPopup('open-popup-consult', 'popupConsult');
+    openPopup('open-popup-smeta', 'popupSmeta');
+    openPopup('open-popup-catalog', 'popupCatalog');
+    var closeButtons = document.querySelectorAll(".popup__close");
+    closeButtons.forEach(function (el) {
+      el.addEventListener("click", function () {
+        var parent = el.closest(".popup");
+        parent.classList.remove("active");
+      });
+    });
+    document.addEventListener("keydown", function (event) {
+      if (event.keyCode === 27) {
+        document.querySelector(".popup.active").classList.remove("active");
+      }
     });
   }
 });
